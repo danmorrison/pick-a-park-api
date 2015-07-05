88bc0a2b518accb2d6f75c611b5d1b868ed4f617
Tony Allan <tony@apms.com.au>
2015-07-05 12:06:43 +1000

    test api

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # pick-a-park-api
+
+
+```
+logging
+azure site log tail pick-a-park-api --path http
+azure site log tail pick-a-park-api 
+https://azure.microsoft.com/en-us/documentation/articles/web-sites-enable-diagnostic-log/
+
+
+example...
+
+http://pickapark.azurewebsites.net/api?latitude=123&longitude=234&feature=abc
+
+```
--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 var http = require('http');
-http.createServer(function(req,res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  console.log("Hello World");
-  res.end('Hello from Azure running node version: ' + process.version + '</br>');
-}).listen(process.env.PORT || 3000);
-console.log('Server running.');
+var express = require('express');
+ 
+var app = express();
+app.set('port', process.env.PORT || 3000); 
+ 
+app.get('/', function (req, res) {
+  res.send('<html><body><h1>Hello World</h1><p>home of pick-a-park-api<br>use <pre>/api?latitude=123&longitude=234&feature=abc</pre></body></html>');
+});
+
+app.get('/api:latitude?/:longitude?/:feature?', function (req,res) {
+  res.send(req.params.latitude + ' ' + req.params.longitude + ' ' + req.params.feature);
+});
+ 
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
