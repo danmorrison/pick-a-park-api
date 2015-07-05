ac990d80a3a604b624717ac65053ee956e9309f4
Tony Allan <tony@apms.com.au>
2015-07-05 11:47:06 +1000

    logging

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var http = require('http');
 http.createServer(function(req,res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
+  console.log("Hello World");
   res.end('Hello from Azure running node version: ' + process.version + '</br>');
 }).listen(process.env.PORT || 3000);
+console.log('Server running.');
